2e95a2c903d35b2fa7b8dbd6c4e81f3e6ea15f45
Lokeshrajkp <kplokeshraj17@gmail.com>
2023-04-16 11:45:56 +0530

    yolov5

---
+++ template.py
@@ -0,0 +1,56 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s],%(message)s:')
+
+
+project_name='signLanguage'
+
+list_of_files=[
+    '.github/workflows/.gitkeep',
+    'data/.gitkeep',
+    'docs/.gitkeep',
+    f'{project_name}/__init__py',
+    f'{project_name}/components/__init__.py',
+    f'{project_name}/components/data_ingestion.py',
+    f'{project_name}/components/data_valiation.py',
+    f'{project_name}/components/model_trainer.py',
+    f'{project_name}/components/model_pusher.py',
+    f'{project_name}/configuration/__init__.py',
+    f'{project_name}/configuration/s3_operations.py',
+    f'{project_name}/constant/__init__.py',
+    f'{project_name}/constant/training_pipeline/__init__.py',
+    f'{project_name}/constant/application.py',
+    f'{project_name}/entity/__init__.py',
+    f'{project_name}/entity/artifacts_entity.py',
+    f'{project_name}/entity/config_entity.py',
+    f'{project_name}/exception/__init__.py',
+    f'{project_name}/logger/__init__.py',
+    f'{project_name}/pipeline/__init__.py',
+    f'{project_name}/pipeline/training_pipeline.py',
+    f'{project_name}/utils/__init__.py',
+    f'{project_name}/utils/main_utils.py', 
+    'template/index.html',
+    '.dockerignore',
+    'app.py',   
+    'Dockerfile',
+    'requirements.txt',
+    'setup.py'
+]
+
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    file_dir,file_name=os.path.split(filepath)
+
+    if file_dir !='':
+        os.makedirs(file_dir,exist_ok=True)
+        logging.info(f'creating directory:{file_dir} for file {file_name}')
+
+    if (not os.path.exists(file_name)) or (os.path.getsize(file_name)==0):
+        with open(filepath,'w')as f:
+            pass
+            logging.info(f'creating empty file:{file_name}')  
+    else:
+        logging.info(f'{file_name} already created')
